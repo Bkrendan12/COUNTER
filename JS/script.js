31d6a81a324bd0a74783781eb1e459ddb349eaea
Brendan <bkrendan12@gmail.com>
2022-01-04 15:45:13 -0500

    still troubleshooting the reverse method and took out the for loop. The issue is we have an if statement saying if a number is less than this number, count up until you reach it. the else statement says if it reaches that desired number, count down. but after you countdown one, its not following the if statment so everytime is goes down, it flips and goes right back to 15

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,5 @@
 var count1 = 0;
 var count2 = 0;
-var up = true
-
 
 var upButton = document.querySelector(".up-btn");
 var downButton = document.querySelector(".down-btn");
@@ -9,93 +7,22 @@ var downButton = document.querySelector(".down-btn");
 var counter1 = document.querySelector(".count-1");
 var counter2 = document.querySelector(".count-2");
 
-// upButton.addEventListener("click", () => {
-//   counter1.innerHTML = count1;
-//   PerformCalc1();
-// });
-
-// downButton.addEventListener("click", () => {
-//   counter2.innerHTML = count2;
-//   PerformCalc2();
-// });
-
 upButton.addEventListener("click", () => {
-  count1++;
-  counter1.innerHTML = count1;
-  for (let i = 0; i <= 1; i++) {
-    if (count1 <= 15) {
-      count1++
+  if (count1 === 15) {
+    count1--;
+    upButton.style.transform = "rotate(180deg)";
+  } else {
+    count1++;
   }
-    else if (count1 === 15) {
-      count1--
-      upButton.style.transform = "rotate(180deg)";
-    }
-}
+  counter1.innerHTML = count1;
 });
-
 
 downButton.addEventListener("click", () => {
-  count2--;
-  counter1.innerHTML = count2;
-  for (let i = 0; i <= 1; i--) {
-    if (count2 > -15) {
-      count2--;
-      upButton.style.transform = "rotate(180deg)";
+  if (count2 > -15) {
+    count2--;
+  } else {
+    count2++;
+    downButton.style.transform = "rotate(180deg)";
   }
-    else if (count2 === -15) {
-      count2++
-    }
-}
+  counter2.innerHTML = count2;
 });
-
-// function PerformCalc1() {
-//   if (upButton == true && count1 <= 15) {
-//     count1++
-
-//     if (count1 === 15) {
-//       upButton = false;
-//     }
-//   } else {
-//       upButton = false
-//       count1--;
-//       upButton.style.transform = "rotate(180deg)";
-
-//       if (count1 == 0) {
-//         upButton = true;
-//       }
-//   }
-// }
-
-  // function PerformCalc2() {
-  //   if (downButton == true && count2 >= -15) {
-  //     count2--
-  
-  //     if (count2 == -15) {
-  //       downButton = false;
-  //     }
-  //   } else {
-  //       downButton = false
-  //       count2++;
-  //       upButton.style.transform = "rotate(180deg)";
-  
-  //       if (count2 == 0) {
-  //         upButton = true;
-  //       }
-  //   }
-  // }
-
-
-
-// function flip1() {
-//   if (count1 >= 15) {
-//     count1--;
-   
-//   }
-// }
-
-// function flip2() {
-//   if (count2 <= -15) {
-//     count2++;
-//     downButton.style.transform = "rotate(180deg)";
-//   }
-// }
